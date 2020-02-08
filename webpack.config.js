29ecf0924a0e5bcc02c68bdb76120d36997431f2
valovm <valov.mikhail.a@gmail.com>
2020-02-08 13:32:29 +0300

    Fix Assets Path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist',
+        contentBase: './public/dist',
         hot: true,
     },
     module: {
